fcc44a140ace5bd1bb2fbf112897874bec62994b
kaylab78 <kaylabackus78@gmail.com>
2022-07-17 11:59:06 -0500

    Created PUT route for Tag.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -63,6 +63,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData[0]) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
