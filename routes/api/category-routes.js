76f2d4d0861d6871ebd1fd1994060dd77febd5a4
kaylab78 <kaylabackus78@gmail.com>
2022-07-17 09:42:27 -0500

    Used module 13 to write POST route for Category.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,6 +37,14 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+   category_name: req.body.category_name
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
