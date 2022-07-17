06bf589ea40c178bdaa9af3f2672865ec23f272b
kaylab78 <kaylabackus78@gmail.com>
2022-07-17 08:09:05 -0500

    Created columns for ProductTag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -4,9 +4,29 @@ const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
+// columns for ProductTag model
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
