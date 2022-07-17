cf409d71fa71fd47b6f2ea8e14b117f5e9871412
kaylab78 <kaylabackus78@gmail.com>
2022-07-17 15:26:26 -0500

    Stuck on PUT route for Product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -97,10 +97,19 @@ router.post('/', (req, res) => {
     });
 });
 
+// HELP
 // update product
 router.put('/:id', (req, res) => {
   // update product data
-  Product.update(req.body, {
+  // Product.update(req.body, {
+  Product.update(
+    {
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      tagIds: req.body.tagIds
+    }, 
+    {
     where: {
       id: req.params.id,
     },
