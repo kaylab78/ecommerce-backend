3acb1a1eaa807520fdda8f6caaaf486536ab33e7
kaylab78 <kaylabackus78@gmail.com>
2022-07-17 12:13:48 -0500

    Created DELETE route for Tag.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -83,6 +83,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
