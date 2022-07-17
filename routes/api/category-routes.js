5b18d2daedc742752ee7f38802b3de3e5cedc0da
kaylab78 <kaylabackus78@gmail.com>
2022-07-17 09:59:33 -0500

    Referenced module 13 to create a DELETE route for Category.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { UPSERT } = require('sequelize/types/query-types');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
@@ -69,6 +70,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
