e702e053222744f94909ae99cefd2ca589d8dbde
kaylab78 <kaylabackus78@gmail.com>
2022-07-17 14:52:41 -0500

    Created GET route for all Products.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,15 +5,25 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // Product.findAll({
-  //   attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
-  //   include: [{
-  //     model: Category,
-  //     attributes: ['id', 'category_name']
-  //   }]
-  // })
-  // be sure to include its associated Category and Tag data
+  // find all products, including associated Category and Tag data.
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
